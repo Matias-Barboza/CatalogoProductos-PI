ce9ad4d65f80d14a5fbd64e466af6abfd7e50851
Matias Barboza <matiasbarboza540@gmail.com>
2024-06-18 11:43:12 -0300

    Navbar & Footer

--- CatalogoProductos-dominio/Articulo.cs
+++ CatalogoProductos-dominio/Articulo.cs
@@ -8,6 +8,7 @@ namespace CatalogoProductos_dominio
 {
     public class Articulo
     {
+        public int Id { get; set; }
         public int CodigoArticulo { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
